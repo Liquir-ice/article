bac964def5fc1f3928f9155bbd896b576b17a469
lukeboh <lukeboh@kkbox.com>
2015-06-19 17:36:36 +0800

    add PagesController@about

---
+++ app/Http/Controllers/PagesController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+
+class PagesController extends Controller
+{
+    public function about()
+    {
+
+        $name = 'luke <span style="color: red;">boh</span>';
+        // return view('pages/about')->with('name', $name);
+
+
+        // return view('pages/about')->with([
+        //     'first' => 'luke',
+        //     'last' => 'boh',
+        //     'name' => $name
+        // ]);
+
+
+        // $data = [];
+        // $data['first'] = 'luke';
+        // $data['last'] = 'boh';
+        // $data['name'] = $name;
+        // return view('pages/about')->with($data);
+
+        // compact : Create array containing variables and their values
+        $first = 'luke';
+        $last = 'boh';
+        $name = $name;
+        return view('pages/about', compact('first', 'last', 'name'));
+
+
+    }
+}
