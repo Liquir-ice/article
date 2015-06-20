7012151b5a6c5d270be22f7fe87b2a70946bb4d5
lukeboh <lukeboh@kkbox.com>
2015-06-21 01:10:32 +0800

    add require bootstrap css

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -3,6 +3,8 @@
 <head>
     <meta charset="UTF-8">
     <title>Document</title>
+    <!-- load bootstrap from a cdn -->
+    <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
 </head>
 <body>
 
