214456400846d591ca7b1b38a30b6b3179113c9d
lukeboh <lukeboh@kkbox.com>
2015-06-21 13:24:47 +0800

    add query scope and protected dates

--- app/Article.php
+++ app/Article.php
@@ -4,6 +4,8 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
+use Carbon\Carbon;
+
 class Article extends Model
 {
     protected $fillable = [
@@ -15,4 +17,24 @@ class Article extends Model
         'published_at'
 
     ];
+
+    // any timestamp treat as carbon
+    protected $dates = ['published_at'];
+
+    public function scopePublished($query)
+    {
+        $query->where('published_at', '<=', Carbon::now());
+    }
+
+    public function scopeUnPublished($query)
+    {
+        $query->where('published_at', '>=', Carbon::now());
+    }
+
+    // setNameAttribute
+    public function setPublishedAtAttribute($date)
+    {
+        // $this->attributes['published_at'] = Carbon::createFromFormat('Y-m-d', $date);
+        $this->attributes['published_at'] = Carbon::parse($date);
+    }
 }
