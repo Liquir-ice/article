6959d69292d0f615043923f8144b32730aa9e4a0
lukeboh <lukeboh@kkbox.com>
2015-06-21 14:51:09 +0800

    show validation error

--- resources/views/articles/create.blade.php
+++ resources/views/articles/create.blade.php
@@ -42,4 +42,12 @@
 
     {!! Form::close() !!}
 
+    <!-- {{ var_dump($errors) }} -->
+    @if ($errors->any())
+        <ul class="alert alert-danger">
+            @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+    @endif
 @stop
