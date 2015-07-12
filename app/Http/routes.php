2f66de1ac6f308f01b3e5bf48ae5239d586176fe
lukeboh <lukeboh@kkbox.com>
2015-07-12 14:03:58 +0800

    add auth routes

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -48,3 +48,8 @@ Route::resource('articles', 'ArticlesController');
 // {
 //     return 'bar';
 // });
+
+Route::controllers([
+    'auth' => 'Auth\AuthController',
+    'password' => 'Auth\PasswordController'
+]);
