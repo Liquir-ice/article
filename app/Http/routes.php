77ef05d5d4ee734e74d68470f379374b60fb0003
lukeboh <lukeboh@kkbox.com>
2015-07-12 15:14:56 +0800

    update route with manager middleware

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -53,3 +53,7 @@ Route::controllers([
     'auth' => 'Auth\AuthController',
     'password' => 'Auth\PasswordController'
 ]);
+
+Route::get('foo', ['middleware' => 'manager', function(){
+    return 'this page may only be view by manager';
+}]);
